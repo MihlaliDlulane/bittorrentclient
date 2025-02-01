42d3c3631658044cb7a02b62b8562fdffca919d2
mihlali800 <Mihlali800@gmail.com>
2025-02-01 18:58:47 +0200

    [any message]

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-use serde_json;
 use std::env;
-
-// Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> i32 {
+
+    let string_ecode = std::string::String::from(encoded_value);
+    let uncoded_value = string_ecode.trim_matches(['i','e']);
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
+    if uncoded_value.chars().next().unwrap().is_digit(10) {
+        return uncoded_value.parse::<i32>().unwrap();
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
@@ -25,13 +20,9 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        eprintln!("Logs from your program will appear here!");
-
-        // Uncomment this block to pass the first stage
         let encoded_value = &args[2];
         let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        println!("decoded value:{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
